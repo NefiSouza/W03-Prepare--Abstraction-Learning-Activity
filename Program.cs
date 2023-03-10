d309c857bb8b8ef2229fd6a87567d410d407654d
NefiSouza <122121591+NefiSouza@users.noreply.github.com>
2023-03-10 16:37:11 -0700

    change 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job1 = new Job(); 
+        job1._company = "Fairfeild";
+        job1._jobTitle = "Housekeeer";
+        job1._startYear = 1995;
+        job1._endYear = 2001; 
+
+        Job job2 = new Job(); 
+        job2._company = "Country Blooms";
+        job2._jobTitle = "Hired hand";
+        job2._startYear = 2006; 
+        job2._endYear = 2012;
+
+        Resume resume1 = new Resume(); 
+        resume1._name = "Nefi Souza";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+    }
+}
